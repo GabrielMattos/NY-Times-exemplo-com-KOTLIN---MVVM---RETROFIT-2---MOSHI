b7fb12e30931dbb7ecb35d6baac656da3d41ab16
GABRIEL MATTOS DA SILVA SOUSA <gabriel.mattos17@hotmail.com>
2020-03-29 13:46:46 -0300

    Curso de Android com Kotlin e MVVM #7 - Finalizando a Camada de Network (Mapeamento da API)

--- NYBOOKS/app/src/main/java/br/com/gabrielmattos/nybooks/data/NYTServices.kt
+++ NYBOOKS/app/src/main/java/br/com/gabrielmattos/nybooks/data/NYTServices.kt
@@ -1,11 +1,16 @@
 package br.com.gabrielmattos.nybooks.data
 
 import br.com.gabrielmattos.nybooks.data.model.Book
+import br.com.gabrielmattos.nybooks.data.response.BookBodyResponse
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface NYTServices
 {
     @GET("lists.json")
-    fun listRepos(): Call<List<>>
+    fun getBooks(
+        @Query("api-key") apiKey:String = "13pbqWJpItICGri9WCLQI7CdW0uluZGI",
+        @Query("list") list:String = "hardcover-fiction"
+    ): Call<BookBodyResponse>
 }
