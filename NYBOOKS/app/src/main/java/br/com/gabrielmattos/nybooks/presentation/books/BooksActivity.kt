a7fbb84ed72a959cb8edb559ec7a97e10c6cacd5
GABRIEL MATTOS DA SILVA SOUSA <gabriel.mattos17@hotmail.com>
2020-03-29 11:31:53 -0300

    Curso de Android com Kotlin e MVVM #3 - Adicionando os componentes Toolbar e RecyclerView no projeto

--- NYBOOKS/app/src/main/java/br/com/gabrielmattos/nybooks/presentation/books/BooksActivity.kt
+++ NYBOOKS/app/src/main/java/br/com/gabrielmattos/nybooks/presentation/books/BooksActivity.kt
@@ -3,11 +3,15 @@ package br.com.gabrielmattos.nybooks.presentation.books
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import br.com.gabrielmattos.nybooks.R
+import kotlinx.android.synthetic.main.activity_books.*
 
 class BooksActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_books)
+
+        toolbar_main_ID.title = "Books"
+        setSupportActionBar(toolbar_main_ID)
     }
 }
