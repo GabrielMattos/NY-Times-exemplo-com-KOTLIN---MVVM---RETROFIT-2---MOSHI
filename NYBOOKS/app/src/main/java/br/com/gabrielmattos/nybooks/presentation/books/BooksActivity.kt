f64fa796d31397ddc15611152fc1d139eb2961d1
GABRIEL MATTOS DA SILVA SOUSA <gabriel.mattos17@hotmail.com>
2020-03-29 11:22:18 -0300

    Curso de Android com Kotlin e MVVM #2 - Criando e Organizando o Projeto

---
+++ NYBOOKS/app/src/main/java/br/com/gabrielmattos/nybooks/presentation/books/BooksActivity.kt
@@ -0,0 +1,13 @@
+package br.com.gabrielmattos.nybooks.presentation.books
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.gabrielmattos.nybooks.R
+
+class BooksActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_books)
+    }
+}
