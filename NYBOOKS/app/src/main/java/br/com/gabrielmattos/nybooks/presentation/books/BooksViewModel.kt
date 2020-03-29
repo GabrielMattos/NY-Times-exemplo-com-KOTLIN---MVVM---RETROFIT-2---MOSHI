0b6bb0c2ccff8f7fa828228e6a7a7c896515ea40
GABRIEL MATTOS DA SILVA SOUSA <gabriel.mattos17@hotmail.com>
2020-03-29 13:59:04 -0300

    Curso de Android com Kotlin e MVVM #8 - Data Parser e Exibição da Lista de Livros

--- NYBOOKS/app/src/main/java/br/com/gabrielmattos/nybooks/presentation/books/BooksViewModel.kt
+++ NYBOOKS/app/src/main/java/br/com/gabrielmattos/nybooks/presentation/books/BooksViewModel.kt
@@ -2,7 +2,12 @@ package br.com.gabrielmattos.nybooks.presentation.books
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import br.com.gabrielmattos.nybooks.data.ApiService
 import br.com.gabrielmattos.nybooks.data.model.Book
+import br.com.gabrielmattos.nybooks.data.response.BookBodyResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class BooksViewModel : ViewModel()
 {
@@ -10,15 +15,30 @@ class BooksViewModel : ViewModel()
 
     fun getBooks()
     {
-        booksLiveData.value = createFakeBooks()
-    }
+        ApiService.service.getBooks().enqueue(object:Callback<BookBodyResponse>
+        {
+            override fun onResponse(call: Call<BookBodyResponse>, response: Response<BookBodyResponse>)
+            {
+                if(response.isSuccessful)
+                {
+                    val books:MutableList<Book> = mutableListOf()
+                    response.body()?.let { bookBodyResponse ->
+                        for(result in bookBodyResponse.bookResults)
+                        {
+                            val book = Book(title = result.bookDetails[0].title, author = result.bookDetails[0].author)
+                            books.add(book)
+                        }
+                    }
 
-    fun createFakeBooks() : List<Book>
-    {
-        return listOf<Book>(
-            Book("Title 1", "Author 1"),
-            Book("Title 2", "Author 2"),
-            Book("Title 3", "Author 3")
-        )
+                    booksLiveData.value = books
+                }
+            }
+
+            override fun onFailure(call: Call<BookBodyResponse>, t: Throwable)
+            {
+                TODO("Not yet implemented")
+            }
+
+        })
     }
 }
